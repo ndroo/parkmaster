d03ee93338b15ab3afdcd2116f70aabbe9046f70
Andrew McGrath <andrew.mcgrath@fasttrackit.com.au>
2012-10-15 01:51:43 -0400

    adding example code

---
+++ example-config.php
@@ -0,0 +1,9 @@
+<?php
+
+//default timezone
+define("TIME_ZONE","America/Toronto");
+
+// Twilio API Info
+define("TWILIO_SID", "YOUR_TWILIO_SID_HERE");
+define("TWILIO_TOKEN", "YOUT_TWILIO_TOKEN_HERE");
+define("TWILIO_NUMBER", "TWILIO_NUMBER_YOU_SEND_FROM_HERE");
